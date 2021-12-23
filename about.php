4f67cccb3176e95171a4ccb026a4cbba0c28c622
Aron <aron-dosti@hotmail.com>
2021-12-23 18:19:40 +0100

    Update 3
    Films en series tonen nu een beschrijving , jaartal van publicering en een trailer in de about.php

--- about.php
+++ about.php
@@ -2,9 +2,20 @@
 
 include_once "database/connect.php";
 
+include_once "header.php";
+
 if(isset($_GET['id'])) {
 
-    
+ $sql = $db->prepare("SELECT * FROM movie WHERE id = '".$_GET['id']."' ");
+ $sql->execute();
+ $info = $sql->fetch(PDO::FETCH_ASSOC);
+
+  echo "<div class='title'>".$info['title']."</div><br>";
+  echo "<div class='flexcont'><iframe width='420' height='345' src='".$info['url']."'>
+    </iframe>";
+  echo "<div><h5>Publicatiedatum</h5>".$info['year']."</div>";
+  echo "<div><h2>Beschrijving</h2>".$info['description']."</div>";
+  echo "</div>";
 
 
 }
---
+++ maintenance.php
@@ -0,0 +1,30 @@
+<?php
+header("refresh:5;url=index.php");
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="header">
+        <a href="index.php" class="logo"><img src="logo/NETFISH_LogoWebsite.jpg" class="logo-img"></a>
+    </div>
+
+        <div class="middle">
+            <h1>COMING SOON</h1>
+            <hr>
+            <p>Returning back to the homepage...</p>
+        </div>
+
+
+    </body>
+
+</html>
